f8ca7eabdce8e679e7831055018841fe250f0245
seorang42 <seorang42@naver.com>
2025-04-06 19:36:40 +0900

    11-6 Entity Embedding

---
+++ src/entity/person.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Name {
+  @Column()
+  first: string;
+
+  @Column()
+  last: string;
+}
+
+@Entity()
+export class StudentModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column(() => Name)
+  name: Name;
+
+  @Column()
+  class: string;
+}
+
+@Entity()
+export class TeacherModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // name + first, name + last
+  @Column(() => Name)
+  name: Name;
+
+  @Column()
+  salary: number;
+}

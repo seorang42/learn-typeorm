24c43086eb6bfc5c946645f019c09054c6ecc8d2
seorang42 <seorang42@naver.com>
2025-04-30 18:21:23 +0900

    11-14 Typeorm 유틸리티 탐구

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,20 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role, UserModel } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import {
+  Between,
+  Equal,
+  ILike,
+  In,
+  IsNull,
+  LessThan,
+  LessThanOrEqual,
+  Like,
+  MoreThan,
+  MoreThanOrEqual,
+  Not,
+  Repository,
+} from 'typeorm';
 import { ProfileModel } from './entity/profile.entity';
 import { PostModel } from './entity/post.entity';
 import { TagModel } from './entity/tag.entity';
@@ -28,49 +41,70 @@ export class AppController {
   ) {}
 
   @Post('users')
-  postUser() {
-    return this.userRepository.save({
-      email: '1234@gmail.com',
-    });
+  async postUser() {
+    for (let i = 0; i < 100; i++) {
+      await this.userRepository.save({
+        email: `user-${i}@gmail.com`,
+      });
+    }
   }
 
   @Get('users')
   getUsers() {
     return this.userRepository.find({
+      // 필터링할 조건을 입력
+      where: {
+        // 조건이 아닌 경우
+        // id: Not(1),
+        // 조건보다 적은 경우
+        // id: LessThan(30),
+        // 조건보다 적거나 같은 경우
+        // id: LessThanOrEqual(30),
+        // 조건보다 많은 경우
+        // id: MoreThan(30),
+        // 조건보다 많거나 같은 경우
+        // id: MoreThanOrEqual(30),
+        // 같은 경우
+        // id: Equal(30),
+        // 유사값
+        // email: Like('%GMAIL%'),
+        // 대문자, 소문자 구별하지 않는 유사값
+        // email: ILike('%GMAIL%'),
+        // 사이값
+        // id: Between(10, 20),
+        // 해당되는 여러 개의 값
+        // id: In([1, 3, 7, 10, 99]),
+        // null인 경우
+        // id: IsNull(),
+      },
+
       // 어떤 속성을 선택할지
       // select를 정의하지 않으면(select: {}) 모든 속성을 가져온다
       // select를 정의하면 정의된 속성만 가져오게 된다
-      select: {
-        id: true,
-        createdAt: true,
-        updatedAt: true,
-        version: true,
-        profile: {
-          id: true,
-        },
-      },
-
-      // 필터링할 조건을 입력
-      where: {
-        // profile: {
-        //   id: 3,
-        // },
-      },
+      // select: {
+      //   id: true,
+      //   createdAt: true,
+      //   updatedAt: true,
+      //   version: true,
+      //   profile: {
+      //     id: true,
+      //   },
+      // },
       // 관계를 가져오는 법
       // relation을 가져오면 select, where 등에서도 사용 가능
-      relations: {
-        profile: true,
-      },
+      // relations: {
+      //   profile: true,
+      // },
       // 오름차, 내림차순 정렬
       // ASC -> 오름차
       // DESC -> 내림차
-      order: {
-        id: 'DESC',
-      },
+      // order: {
+      //   id: 'DESC',
+      // },
       // 정렬 후 처음 몇 개를 제외할지 선택(기본값은 0)
-      skip: 0,
+      // skip: 0,
       // 정렬 후 처음 몇 개를 가져올지 선택(기본값은 0)
-      take: 2,
+      // take: 2,
     });
   }
 

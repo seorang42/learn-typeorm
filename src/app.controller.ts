47d3e0e59e99707512c9bfe2ccca7a1469464c6b
seorang42 <seorang42@naver.com>
2025-04-30 16:59:42 +0900

    11-11 Many to Many Relationship 구현해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,6 +11,7 @@ import { Role, UserModel } from './entity/user.entity';
 import { Repository } from 'typeorm';
 import { ProfileModel } from './entity/profile.entity';
 import { PostModel } from './entity/post.entity';
+import { TagModel } from './entity/tag.entity';
 
 @Controller()
 export class AppController {
@@ -21,6 +22,8 @@ export class AppController {
     private readonly profileRepository: Repository<ProfileModel>,
     @InjectRepository(PostModel)
     private readonly postRepository: Repository<PostModel>,
+    @InjectRepository(TagModel)
+    private readonly tagRepository: Repository<TagModel>,
   ) {}
 
   @Post('users')
@@ -86,5 +89,51 @@ export class AppController {
     });
 
     return user;
+  }
+
+  @Post('posts/tags')
+  async createPostsTags() {
+    const post1 = await this.postRepository.save({
+      title: 'NestJS Lecture',
+    });
+
+    const post2 = await this.postRepository.save({
+      title: 'TypeOrm Lecture',
+    });
+
+    const tag1 = await this.tagRepository.save({
+      name: 'JavaScript',
+      posts: [post1, post2],
+    });
+
+    const tag2 = await this.tagRepository.save({
+      name: 'TypeScript',
+      posts: [post1],
+    });
+
+    const post3 = await this.postRepository.save({
+      title: 'NextJS Lecture',
+      tags: [tag1, tag2],
+    });
+
+    return true;
+  }
+
+  @Get('posts')
+  getPosts() {
+    return this.postRepository.find({
+      relations: {
+        tags: true,
+      },
+    });
+  }
+
+  @Get('tags')
+  getTags() {
+    return this.tagRepository.find({
+      relations: {
+        posts: true,
+      },
+    });
   }
 }
